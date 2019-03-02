c29ecd18020c5ebde390eb69eba29c9f254e6762
chenxi1900 <gonghuiyun@buaa.edu.cn>
2019-03-02 16:47:35 +0800

    17-fix

--- 17/Meta.py
+++ 17/Meta.py
@@ -3,24 +3,36 @@
 发动机编号(engine_number)及载客量(capacity)三个基本属性，没有就不行
 '''
 
+
 class CarMeta(type):
-    #创建类
+    # 创建类
     def __init__(self, class_name, class_bases, class_dic):
-        # super(CarMeta,self).__init__(class_name, class_bases, class_dic)
-        pass
+        if not class_dic.get('production_date'):
+            raise TypeError('production_date ')
+
+        if not class_dic.get('engine_number'):
+            raise TypeError('engine_number')
+
+        if not class_dic.get('capacity'):
+            raise TypeError('capacity')
+
+        super(CarMeta, self).__init__(class_name, class_bases, class_dic)
+
     def __call__(self, *args, **kwargs):
         obj = object.__new__(self)
         self.__init__(obj, *args, **kwargs)
         return obj
 
-class Car1(object,metaclass= CarMeta):
-    def __init__(self, production_date, engine_number, capacity):
-        self.production_date = production_date
-        self.engine = engine_number
-        self.capacity = capacity
 
+class Car1(object, metaclass=CarMeta):
 
-#调用类，在__call__中传入Car1,并且传入参数。
-# 然后创建空对象，调用对象的__init__方法
-# 并用参数对这个对象初始化
+    def __init__(self):
+        pass
+
+    def production_date(self):
+        pass
+
+    def engine_number(self):
+        pass
+
 obj = Car1()
